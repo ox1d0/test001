0748768d3e88aa82ab00c47ad19125287050327e
ox1d0 <4569638-ox1d0@users.noreply.gitlab.com>
2020-08-16 16:07:49 -0500

    adding Flask app

---
+++ flask002/application/routes.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from application import app
+from flask import render_template
+
+
+#df1 = pd.read_csv('../testlog_1/testlog_1', header = None, delimiter='\t', skiprows=[0,1])
+#columns1 = ['date','time','x_edge_location','sc_bytes','c_ip','cs_method','cs_Host','cs_uri_stem','sc_status','cs_Referer','cs_UserAgent)','csuriquery_cs_uri_query','cs_Cookie_cs_Cookie','x_edge_result_typex_edge_result_type','x_edge_request_id','x_host_header','cs_protocol','cs_bytes','time_taken','x_forwarded_for','ssl_protocol','ssl_cipher','x_edge_response_result_type','cs_protocol_version','fle_status','fle_encrypted_fields','c_port','time_to_first_byte','x_edge_detailed_result_type','sc_content_type','sc_content_len','sc_range_start','sc_range_end']
+#df1.columns = columns1
+
+#df2 = pd.read_csv('../testlog_2/testlog_2', header = None, delimiter='\t', skiprows=[0,1])
+#columns2 = ['date','time','x_edge_location','sc_bytes','c_ip','cs_method','cs_Host','cs_uri_stem','sc_status','cs_Referer','cs_UserAgent)','csuriquery_cs_uri_query','cs_Cookie_cs_Cookie','x_edge_result_typex_edge_result_type','x_edge_request_id','x_host_header','cs_protocol','cs_bytes','time_taken','x_forwarded_for','ssl_protocol','ssl_cipher','x_edge_response_result_type','cs_protocol_version','fle_status','fle_encrypted_fields','c_port','time_to_first_byte','x_edge_detailed_result_type','sc_content_type','sc_content_len','sc_range_start','sc_range_end']
+#df2.columns = columns2
+
+#df_agregate = pd.concat([df1,df2],axis=0, keys=['date','time','x-edge-location','sc-bytes','c-ip','cs-method','cs(Host)','cs-uri-stem','sc-status','cs(Referer)','cs(User-Agent)','cs-uri-querycs-uri-query','cs(Cous','fle-encrypted-fields','c-port','time-to-first-byte','x-edge-detailed-result-type','sc-content-type','sc-ontent-len','sc-range-start','sc-range-end'])
+#agg1 = ['date','time','x_edge_location','sc_bytes','c_ip','cs_method','cs_Host','cs_uri_stem','sc_status','cs_Referer','cs_UserAgent)','csuriquery_cs_uri_query','cs_Cookie_cs_Cookie','x_edge_result_typex_edge_result_type','x_edge_request_id','x_host_header','cs_protocol','cs_bytes','time_taken','x_forwarded_for','ssl_protocol','ssl_cipher','x_edge_response_result_type','cs_protocol_version','fle_status','fle_encrypted_fields','c_port','time_to_first_byte','x_edge_detailed_result_type','sc_content_type','sc_content_len','sc_range_start','sc_range_end']
+#df_agregate.columns = agg1
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", login=False)
+
+@app.route('/user/<name>')
+def user(name):
+        return '<h1>Hello, {}!</h1>'.format(name)
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000, debug=False)
